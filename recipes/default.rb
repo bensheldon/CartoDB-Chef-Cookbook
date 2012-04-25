b54a639d5017e0f217559e99a144d3bd198d3ba5
Ben Sheldon <bensheldon@gmail.com>
2012-04-25 06:01:33 -0700

    Have to install zlib before rvm-ing Ruby.

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,6 +17,7 @@ cd /home/ubuntu/downloads
 bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile
 source ~/.bash_profile
+rvm pkg install zlib
 rvm install 1.9.2
 rvm use 1.9.2
 rvm use 1.9.2 --default
